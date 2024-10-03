3eeb43d06074c0d646e001a7544afcfccdfb0011
Taylor Schmidt <taylor.j.schmidt@gmail.com>
2024-10-02 22:20:14 -0400

    adds credential file to gitignore; starts test main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/schmidthole/ibkr-webapi-go/ibkr"
+	"log"
+)
 
 func main() {
-	fmt.Println("hell")
+	log.Println("ibkr web client test")
+
+	oauth, err := ibkr.NewIbkrOAuthContextFromFile("./cmd/credentials.yml")
+	if err != nil {
+		log.Printf("error initializing oauth context: %v", err)
+		panic(err)
+	}
+	log.Printf("oauth context initialized")
+
+	client := ibkr.NewIbkrWebClient(ibkr.ProdBaseUrl, oauth)
+	log.Printf("client initialized, baseurl: %v", client.BaseUrl)
 }
